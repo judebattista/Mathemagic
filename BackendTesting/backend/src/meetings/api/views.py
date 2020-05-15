cc576b0905179f18dcecefd1e6a15a7c975fa7b1
Cameron Rutherford <cameron.rutherford@me.com>
2020-05-15 00:45:01 -0700

    Working on the Algorithm View now.....

--- BackendTesting/backend/src/meetings/api/views.py
+++ BackendTesting/backend/src/meetings/api/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from AlgortihmTesting import 
+#from AlgortihmTesting import 
 
 class MeetingViewSet(viewsets.ModelViewSet):
     serializer_class = MeetingSerializer
