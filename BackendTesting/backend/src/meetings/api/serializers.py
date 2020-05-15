8d751360361029cd9f6cac5885f70c75a3db846f
Cameron Rutherford <cameron.rutherford@me.com>
2020-05-15 00:06:35 -0700

    Oops.....

--- BackendTesting/backend/src/meetings/api/serializers.py
+++ BackendTesting/backend/src/meetings/api/serializers.py
@@ -8,3 +8,11 @@ class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
         fields = ('date', 'startTime', 'endTime', 'title', 'description', 'people')
+
+
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'id')
--- BackendTesting/backend/src/meetings/api/views.py
+++ BackendTesting/backend/src/meetings/api/views.py
@@ -1,7 +1,7 @@
 # https://www.django-rest-framework.org/api-guide/viewsets/
 
 from meetings.models import Meeting
-from .serializers import MeetingSerializer
+from .serializers import MeetingSerializer, UserSerializer
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from rest_framework.response import Response
@@ -9,6 +9,7 @@ from rest_framework.decorators import action
 
 class MeetingViewSet(viewsets.ModelViewSet):
     serializer_class = MeetingSerializer
+    
     queryset = Meeting.objects.all()
 
     @action(detail=False) # default method is get, and detail=False implies a set of things to return
@@ -51,13 +52,14 @@ class MeetingViewSet(viewsets.ModelViewSet):
     # This is definitely insecure, and should be changed
     @action(methods=['get'], detail=False)
     def all_users(self, request, pk=None):
+        user_serializer = UserSerializer
         res = User.objects.all()
         page = self.paginate_queryset(res)
         if page is not None:
-            serializer = User.get_serializer(page, many=True)
+            serializer = user_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer = User.get_serializer(res, many=True)
+        serializer = user_serializer(res, many=True)
         return Response(serializer.data)
 
 
